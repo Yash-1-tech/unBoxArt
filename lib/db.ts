60fb8c77a81d6f4a6dff74776629f6298a3195d4
Yash-1-tech <131190957+Yash-1-tech@users.noreply.github.com>
2026-09-12 10:51:24 +0000

    fix: MongoDB Atlas SRV connection fails due to Node.js DNS resolver

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import dns from 'node:dns/promises';
 import mongoose from 'mongoose';
 import dns from 'node:dns/promises';
 
-dns.setServers(['1.1.1.1', '8.8.8.8']);)
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 dns.setServers(['1.1.1.1', '8.8.8.8']);
 
